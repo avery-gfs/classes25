61bcebf6c390c9e36ae1acc8472323bcc6432ced
Avery <anortonsmith@germantownfriends.org>
2025-09-03 12:45:31 -0400

    dog years

---
+++ cs2/dog-years/template.py
@@ -0,0 +1,10 @@
+# Get two numbers as input from the user: their birth year, and
+# their dog's birth year. Calculate and out the user's age in
+# years, the dog's age in years, and the dog's age in dog-years.
+
+print("Your age (years): ")
+print("Your dog's age (years): ")
+print("Your dog's age (dog-years): ")
+
+# Challenge: calculate your approximate age in months, days, hours,
+# minutes, and seconds
---
+++ cs2/dog-years/warm-up.py
@@ -0,0 +1,5 @@
+# Store your birth year in the variable `birthYear`
+
+# Define a variable `age` based on `birthYear`
+
+# Print out "Your age: <age>"
