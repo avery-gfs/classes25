16357bc544a7d476c653be4a8bbbaca444c60246
Avery <anortonsmith@germantownfriends.org>
2025-09-18 12:36:30 -0400

    update

--- cs2/images/scaled/problem.py
+++ cs2/images/scaled/problem.py
@@ -5,7 +5,14 @@ from PIL import Image
 # Load input image
 im = Image.open("bird.png")
 
-# You code goes here
+# Make blank output image with twice the width and height of the original
+output = Image.new(im.mode, (im.width * 2, im.height * 2))
+
+for y in range(im.height):
+  for x in range(im.width):
+    (r, g, b) = im.getpixel((x, y))
+
+    output.putpixel((x, y), (r, g, b))
 
 # Save output image
 output.save("scaled.png")
