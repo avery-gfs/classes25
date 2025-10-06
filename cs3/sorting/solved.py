e5cfe4568b0ed9dca87aa82849d1ec5f16cc496e
Avery <anortonsmith@germantownfriends.org>
2025-10-06 19:38:53 -0400

    update

--- cs3/sorting/solved.py
+++ cs3/sorting/solved.py
@@ -1,6 +1,15 @@
+# Implement selection sort
+# Don't use the built-in `min` function
+#
+# - Initialize an `index` variable to `0`
+# - Find the smallest number in the list from `index` onward
+# - Swap the smallest number with the number at `index`
+# - Increment `index` by `1`
+# - Repeat until `index` equals the length of the list
+
 def sort(items):
 	for index in range(len(items)):
-		
+
 
 print(sort([1, 4, 2, 7, 3, 1]))
 print(sort([22, 15, 39, 6, 46, 49, 0, 43, 37, 45, 23, 20, 15, 24, 13, 23, 46, 26, 17, 18]))
