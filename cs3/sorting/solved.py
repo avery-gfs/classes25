5a1880d0cea054ff66a25f34ec0e3f099ddf7462
Avery <anortonsmith@germantownfriends.org>
2025-10-06 20:03:55 -0400

    update

--- cs3/sorting/solved.py
+++ cs3/sorting/solved.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Implement selection sort
 # Don't use the built-in `min` function
 #
@@ -10,10 +9,7 @@
 
 def sort(items):
 	for index in range(len(items)):
-=======
-def sort(items):
->>>>>>> master
-
+		
 
 print(sort([1, 4, 2, 7, 3, 1]))
 print(sort([22, 15, 39, 6, 46, 49, 0, 43, 37, 45, 23, 20, 15, 24, 13, 23, 46, 26, 17, 18]))
