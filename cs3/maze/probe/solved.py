9db849006b5cbf2098edeefedd3ff2116065bf54
Avery <anortonsmith@germantownfriends.org>
2025-10-23 08:56:04 -0400

    update

---
+++ cs3/maze/probe/solved.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+maze = """
+█████████████████████████████████
+█A█     █B█         █         █ █
+█ █ ███ █ █ ███ █████ █ █████ █ █
+█ █ █ █ █     █       █ █   █   █
+█ █ █ █ ███████████ ███ █ █ █████
+█   █ █     █     █ █ █   █     █
+█ ███ █████ █ ███ █ █ ███████ █ █
+█ █     █ █ █ █ █ █       █ █ █ █
+█ █ ███ █ █ █ █ █ █████████ ███ █
+█ █   █   █ █   █   █       █   █
+█ ███ ███ █ ███ ███ █ ███████ █ █
+█ █   █ █ █     █ █   █   █   █ █
+█ █ ███ █ ███████ █████ █ █ ███ █
+█ █   █ █       █   █   █ █ █ █ █
+█ ███ █ ███████ █ █ █ ███ █ █ █ █
+█     █           █   █     █   █
+█████████████████████████████████
+"""
+
+board = [list(row) for row in maze.strip().split("\n")]
+
+
+def update(r, c, symbol):
+    board[r][c] = symbol
+    os.system("clear")
+    print("\n".join("".join(row) for row in board))
+    time.sleep(0.05)
+
+
+def probe(r, c):
+    cell = board[r][c]
+
+    if cell == "█" or cell == "▒":
+        return
+
+    update(r, c, "▒")
+
+    probe(r - 1, c)
+    probe(r, c + 1)
+    probe(r + 1, c)
+    probe(r, c - 1)
+
+    update(r, c, " ")
+
+
+probe(1, 1)
---
+++ cs3/maze/warm-up/solved.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+maze = """
+█████████████████████████████████
+█A█     █B█         █         █ █
+█ █ ███ █ █ ███ █████ █ █████ █ █
+█ █ █ █ █     █       █ █   █   █
+█ █ █ █ ███████████ ███ █ █ █████
+█   █ █     █     █ █ █   █     █
+█ ███ █████ █ ███ █ █ ███████ █ █
+█ █     █ █ █ █ █ █       █ █ █ █
+█ █ ███ █ █ █ █ █ █████████ ███ █
+█ █   █   █ █   █   █       █   █
+█ ███ ███ █ ███ ███ █ ███████ █ █
+█ █   █ █ █     █ █   █   █   █ █
+█ █ ███ █ ███████ █████ █ █ ███ █
+█ █   █ █       █   █   █ █ █ █ █
+█ ███ █ ███████ █ █ █ ███ █ █ █ █
+█     █           █   █     █   █
+█████████████████████████████████
+"""
+
+board = [list(row) for row in maze.strip().split("\n")]
+
+
+def update(r, c, symbol):
+    board[r][c] = symbol
+    os.system("clear")
+    print("\n".join("".join(row) for row in board))
+    time.sleep(0.05)
+
+
+def fill(r, c):
+    cell = board[r][c]
+
+    if cell == "█" or cell == "▒":
+        return
+
+    update(r, c, "▒")
+
+    fill(r - 1, c)
+    fill(r, c + 1)
+    fill(r + 1, c)
+    fill(r, c - 1)
+
+
+fill(1, 1)
