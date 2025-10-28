addbfd9114b954cc14390610c8339f971b4b912c
Avery <anortonsmith@germantownfriends.org>
2025-10-28 11:55:13 -0400

    update problems

--- cs3/maze/search/problem.py
+++ cs3/maze/search/problem.py
@@ -31,6 +31,19 @@ def update(r, c, symbol):
     time.sleep(0.05)
 
 
+# Our original probe function didn't return any value. It also didn't
+# stop when it reached the goal, and erased every path it explored,
+# including the one that found the goal. Our search function will be
+# similar to probe, but it should:
+#
+# 1) Return `True` if the search branch(es) starting from a given (r, c)
+#    position found the goal, and `False` otherwise
+#
+# 2) Stop searching once the goal is reached
+#
+# 3) Not erase the succeeding path
+
+
 def search(r, c):
     cell = board[r][c]
 
@@ -42,10 +55,13 @@ def search(r, c):
     if cell == "B":
         return True
 
+    # Use the return value from the child search calls to decide
+    # whether to keep searching / whether to erase the path
+
     search(r - 1, c)
-    search(r, c + 1)
     search(r + 1, c)
     search(r, c - 1)
+    search(r, c + 1)
 
     update(r, c, " ")
 
