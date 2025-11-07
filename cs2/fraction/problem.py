c881e46bc5d11c3be6cf48c212b4dcfbb5aa3593
Avery <anortonsmith@germantownfriends.org>
2025-11-07 09:27:49 -0500

    update problems

--- cs2/fraction/problem.py
+++ cs2/fraction/problem.py
@@ -1,17 +1,8 @@
-# https://en.wikipedia.org/wiki/Greatest_common_divisor
-
-
-def gcd(a, b):
-    while b != 0:
-        tmp = b
-        b = a % b
-        a = tmp
-    return a
+import math
 
 
 class Fraction:
     def __init__(self, num, den):
-        # Challenge: automatically simplify fractions
         self.num = num
         self.den = den
 
