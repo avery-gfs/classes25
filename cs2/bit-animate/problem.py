6ff73bfe78f51742fb49b33a968ffbb31399140a
Avery <anortonsmith@germantownfriends.org>
2026-02-20 15:25:35 -0500

    update problems

--- cs2/bit-animate/problem.py
+++ cs2/bit-animate/problem.py
@@ -47,8 +47,8 @@ import demos
 # animate(demos.smile, 2)
 # animate(demos.gfs, 4)
 # animate(demos.arrow, 10)
-animate(demos.bounce, 10)
-# animate(demos.snake, 12)
+# animate(demos.bounce, 10)
+animate(demos.snake, 12)
 
 custom = [
     "0042001800427e00",
