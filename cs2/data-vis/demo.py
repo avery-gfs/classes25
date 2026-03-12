9e7ddf7d6897d32cea8a9f8e68e1633d464be185
Avery <anortonsmith@germantownfriends.org>
2026-03-12 11:56:20 -0400

    update problems

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -4,4 +4,4 @@ cities = pl.read_csv("cities.csv")  # Load dataframe from CSV
 
 withPopDensity = cities.with_columns((pl.col("pop2024") / pl.col("area")))
 
-print(withPopDensity)
+print(len(cities))
