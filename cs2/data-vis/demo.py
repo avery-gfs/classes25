f8446a21c26522be2666a64187da87bb2448b92a
Avery <anortonsmith@germantownfriends.org>
2026-03-13 10:04:55 -0400

    update

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-cities = pl.read_csv("cities.csv")  # Load dataframe from CSV
+cities = pl.read_csv("data/cities.csv")  # Load dataframe from CSV
 
 withPopChange = cities.with_columns(
     (pl.col("pop2024") - pl.col("pop2020")).alias("change")
