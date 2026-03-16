5e8bfcb3dae6fe854367d8a79bb7e5c68d0116b8
Avery <anortonsmith@germantownfriends.org>
2026-03-16 09:29:36 -0400

    update

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -3,34 +3,15 @@ import altair as alt
 
 cities = pl.read_csv("data/cities.csv")  # Load dataframe from CSV
 
-
-topCities = cities.head(20)
-chart = (
-    alt.Chart(topCities).mark_bar().encode(alt.X("pop2024"), alt.Y("city", sort="-x"))
-)
-chart.save("pop-bars.png", scale_factor=1.5)
-
-
-cityCounts = cities.group_by("state").count()
-
-chart = (
-    alt.Chart(cityCounts).mark_bar().encode(alt.X("state", sort="-y"), alt.Y("count"))
-)
-chart.save("city-counts.png", scale_factor=1.5)
-
-
-## Scatter Plot
-
-top25 = cities.head(25)
-
-chart = (
-    alt.Chart(top25)
-    .mark_circle(size=40)
-    .encode(
-        alt.X("area"),
-        alt.Y("pop2024"),
-        alt.Color("state"),
+pctChange = (
+    cities.with_columns(
+        (pl.col("pop2024") / pl.col("pop2020") * 100 - 100).alias("pctChange")
     )
+    .sort("pctChange", descending=True)
+    .head(10)
 )
 
-chart.save("pop-area-scatter.png", scale_factor=1.5)
+chart = (
+    alt.Chart(pctChange).mark_bar().encode(alt.X("pctChange"), alt.Y("city", sort="-x"))
+)
+chart.save("pct-change.png", scale_factor=1.5)
