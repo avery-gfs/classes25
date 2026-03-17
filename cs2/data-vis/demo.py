4e5827712bcab88f01b743f90b16a14ed6e455b7
Avery <anortonsmith@germantownfriends.org>
2026-03-17 13:05:28 -0400

    update problems

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -3,18 +3,4 @@ import altair as alt
 
 cities = pl.read_csv("data/cities.csv")  # Load dataframe from CSV
 
-print(
-    (
-        cities.select(
-            pl.col("city"),
-            pl.col("state"),
-            (pl.col("pop2024") - pl.col("pop2020")).alias("popChange"),
-            ((pl.col("pop2024") / pl.col("pop2020")) * 100 - 100)
-            .round(1)
-            .alias("pctChange"),
-        )
-        .sort("pctChange", descending=True)
-        .head(10)
-        .with_row_index(name="rank", offset=1)
-    )
-)
+print(cities.sort("pop2024", descending=True).group_by("state").first().sort("state"))
