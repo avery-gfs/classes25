79ef4f27e307f4e4bfd613e083d01bb31eb40145
Avery <anortonsmith@germantownfriends.org>
2026-03-19 13:17:28 -0400

    update

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -3,4 +3,13 @@ import altair as alt
 
 cities = pl.read_csv("data/cities.csv")  # Load dataframe from CSV
 
-print(cities.sort("pop2024", descending=True).group_by("state").first().sort("state"))
+print(
+    cities.select(
+        pl.col("city"),
+        pl.col("state"),
+        (pl.col("pop2024") - pl.col("pop2020")).alias("popChange"),
+    )
+    .sort("popChange", descending=True)
+    .head(10)
+    .with_row_index(name="rank", offset=1)
+)
