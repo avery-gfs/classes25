0505e88e889abf6af04ee476ab83fdae122b4294
Avery <anortonsmith@germantownfriends.org>
2026-03-19 14:14:20 -0400

    update

--- cs2/data-vis/demo.py
+++ cs2/data-vis/demo.py
@@ -1,15 +1,17 @@
 import polars as pl
 import altair as alt
 
-cities = pl.read_csv("data/cities.csv")  # Load dataframe from CSV
+cities = pl.read_csv("cities.csv")  # Load dataframe from CSV
 
-print(
-    cities.select(
-        pl.col("city"),
-        pl.col("state"),
-        (pl.col("pop2024") - pl.col("pop2020")).alias("popChange"),
+cityCounts = cities.group_by("state").count()
+
+chart = (
+    alt.Chart(cityCounts)
+    .mark_bar()
+    .encode(
+        alt.X("state", sort="-y"),
+        alt.Y("count"),
     )
-    .sort("popChange", descending=True)
-    .head(10)
-    .with_row_index(name="rank", offset=1)
 )
+
+chart.save("city-counts.png", scale_factor=1.5)
