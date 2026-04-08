768b370190000c45a9d01d200daf2dd86a994a62
Avery <anortonsmith@germantownfriends.org>
2026-04-08 13:19:29 -0400

    update

--- cs3/c/demo.c
+++ cs3/c/demo.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
-    for (int n = 1; n <= 100; n++) {
-        if (n % 15 == 0) {
-            printf("fizz buzz\n");
-        } else if (n % 3 == 0) {
-            printf("fizz\n");
-        } else if (n % 5 == 0) {
-            printf("buzz\n");
-        } else {
-            printf("%d\n", n);
-        }
-    }
+    char greeting[] = "Hello";
+    char name[] = "Avery";
+    greeting[5] = '$';
+    printf("%s %s!\n", greeting, name);
 }
